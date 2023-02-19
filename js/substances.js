195d9089160d0cdd50a88355d7f7b76045244fa5
Happy Hooman <tornea.alexandru@gmail.com>
2023-02-19 16:07:15 +0200

    water flowing down from a specific cell

--- js/substances.js
+++ js/substances.js
@@ -5,9 +5,10 @@ class Playground {
     table = document.querySelector("table");
     frameDelay = 500
     interval = null
+    flowRate = 1
 
     constructor(height, width) {
-        this.arr = Array(height).fill().map(() => Array(width).fill(0))
+        this.arr = Array(height).fill().map(() => Array(width).fill({substance: 'water', amount: 0}))
         this.displayPlayground()
     }
 
@@ -16,31 +17,45 @@ class Playground {
             const row = this.table.insertRow()
             r.forEach(c => {
                 const cell = row.insertCell()
-                cell.innerText = c
+                cell.innerText = c.amount
             })
         })
     }
 
     updateValue(row, cell, value) {
-        this.arr[row][cell] = value
+        this.arr[row][cell].amount = value
         this.table.rows[row].cells[cell].innerText = value
     }
 
-    changeColor(row, column, color) {
-        const cell = this.table.rows[row].cells[column];
-        cell.className = color
+    updateSubstance(row, column, substance) {
+        this.arr[row][column].substance = substance
+        this.table.rows[row].cells[column].className = substance
     }
 
     runSimulation() {
-        if(!this.interval) {
+        if (!this.interval) {
             this.interval = setInterval(() => {
-                const val = this.arr[9][0]
-                this.updateValue(9, 0, val -1)
+
+                const val = this.arr[m][n].amount
+                const amt = Math.min(val, this.flowRate)
+                if (val > 0 && this.checkBelow(m, n)) {
+
+                    if (amt === val) this.updateSubstance(m, n, 'none')
+                    this.updateValue(m, n, val - amt)
+                    const belowAmount = this.arr[m + 1][n].amount
+                    if (belowAmount === 0) this.updateSubstance(m + 1, n, 'water')
+                    this.updateValue(m + 1, n, belowAmount + this.flowRate)
+
+                }
 
             }, this.frameDelay)
         } else {
             console.log('already running')
         }
+    }
+
+    checkBelow(row, column) {
+        return (row + 1 < this.arr.length) && (this.arr[row + 1][column].amount < 100)
     }
 
     stopSimulation() {
@@ -51,11 +66,13 @@ class Playground {
     }
 
     reset() {
-        a.updateValue(9, 0, 20)
+        a.updateValue(m, n, 20)
     }
 }
 
 let a = new Playground(10, 20)
 
-a.updateValue(9, 0, 20)
-a.changeColor(9, 0, 'water')
+const [m, n] = [1, 1]
+
+a.updateValue(m, n, 20)
+a.updateSubstance(m, n, 'water')
